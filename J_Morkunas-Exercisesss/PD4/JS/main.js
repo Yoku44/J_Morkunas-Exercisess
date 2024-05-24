2563ee044b01ee765452255fe963c7d000bd52f6
Yoku44 <150247601+Yoku44@users.noreply.github.com>
2024-05-24 22:28:28 +0200

    Add files via upload

---
+++ J_Morkunas-Exercisesss/PD4/JS/main.js
@@ -0,0 +1,15 @@
+let petName;
+let petAge;
+
+petName = "Jon";
+petAge = 40;
+
+console.log("Pet Name: " + petName);
+console.log("Pet Age: " + petAge);
+
+petName = "Binky";
+petAge = 14;
+
+const buildingAddress = "Tilto 1";
+
+console.log("Building Address: " + buildingAddress);
